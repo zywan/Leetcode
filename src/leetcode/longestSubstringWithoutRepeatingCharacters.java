b42293ccf450511414b5ee429754336c412143c5
zhangyu wan <wanzy@bu.edu>
2019-12-17 18:16:44 -0500

    upadte

---
+++ src/leetcode/longestSubstringWithoutRepeatingCharacters.java
@@ -0,0 +1,33 @@
+package leetcode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+// 题号:3
+public class longestSubstringWithoutRepeatingCharacters {
+    /**
+     * Tips: sliding window
+     * Time complexity: O(n)
+     * Space complexity: O(min(m,n))
+     * upper bounded by the size of the string n and the size of the charset/alphabet m
+     * @param s: String
+     * @return length of longest substring
+     * Example
+     * Input: "abcabcbb"
+     * Output: 3
+     * Explanation: The answer is "abc", with the length of 3.
+     */
+    public int lengthOfLongestSubstring(String s) {
+        Map<Character, Integer> map = new HashMap();
+        int res = 0;
+        if (s == null || s.length() == 0) return res;
+        for (int i = 0, j = 0; j < s.length(); j++) {
+            if (map.containsKey(s.charAt(j))) {
+                i = Math.max(map.get(s.charAt(j)) +1, i);
+            }
+            map.put(s.charAt(j),j);
+            res = Math.max(res, j-i+1);
+        }
+        return res;
+    }
+}
---
+++ src/leetcode/splitLinkedListInParts.java
@@ -0,0 +1,46 @@
+package leetcode;
+
+// 题号:725
+public class splitLinkedListInParts {
+
+     // Definition for singly-linked list.
+     public class ListNode {
+         int val;
+         ListNode next;
+
+         ListNode(int x) {
+             val = x;
+         }
+     }
+    /**
+     * Tip:
+     * If there are N nodes in the linked list root,
+     * then there are N/k items in each part,
+     * plus the first N%k parts have an extra item.
+     * Time complexity: O(n+k)
+     * Space complexity: O(max(k,n))
+     */
+    public ListNode[] splitListToParts(ListNode root, int k) {
+        int len = 0;
+        ListNode temp = root;
+        ListNode[] res = new ListNode[k];
+        // first get the length of linked list
+        while(temp!=null) {
+            len++;
+            temp = temp.next;
+        }
+        int n = len/k;
+        int m = len%k;
+        for (int i = 0; i < k; i++) {
+            ListNode dump = new ListNode(0);
+            ListNode tmp = dump;
+            for (int j = 0; j < n + (i < m ? 1 : 0); j++) {
+                tmp.next = new ListNode(root.val);
+                tmp = tmp.next;
+                if (root != null) root = root.next;
+            }
+            res[i] = dump.next;
+        }
+        return res;
+    }
+}
